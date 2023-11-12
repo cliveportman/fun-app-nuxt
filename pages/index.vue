aa9ea33a6fbeb295117d63bddbeb6e2a362e0a57
cliveportman <clive@cliveportman.co.uk>
2023-11-12 19:17:04 +0000

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { data: activity } = await useFetch('https://fun-api-next.vercel.app/api/activity');
+</script>
+
+<template>
+  <ul>
+    <li>{{ activity.name }}</li>
+    <li>{{ activity.distance }}</li>
+    <li>{{ activity.duration }}</li>
+  </ul>
+</template>
